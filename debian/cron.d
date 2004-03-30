16ce330451ce7f7bdde0f86e04dc570cdd4143d7
Philipp Richter <philipp@linbit.com>
2004-03-30 15:31:08 +0000

    Added debian directory.

---
+++ debian/cron.d
@@ -0,0 +1,23 @@
+#
+#  Test if this synchronisation pair is in sync.
+#
+#  csync2 - cluster synchronisation tool, 2nd generation
+#  LINBIT Information Technologies GmbH <http://www.linbit.com>
+#  Copyright (C) 2004  Clifford Wolf <clifford@clifford.at>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+# 0 4 * * * root csync2 -cr / ; csync2 -T <my-hostname> <peer-hostname>
