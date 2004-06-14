969c0778725542f4a8534241b6eb6ded3052153e
Clifford Wolf <clifford@linbit.com>
2004-06-14 05:44:00 +0000

    Renamed configure.in to configure.ac.

--- release.sh
+++ release.sh
@@ -32,8 +32,8 @@ case "$1" in
 	svn co $URL/tags/$PACKAGE-$VERSION ../$PACKAGE-$VERSION
 
 	cd ../$PACKAGE-$VERSION
-	perl -pi -e "s/SNAPSHOT/$VERSION/g" configure.in
-	svn commit -m "Fixed version info in tag $VERSION" configure.in
+	perl -pi -e "s/SNAPSHOT/$VERSION/g" configure.ac; sleep 2
+	svn commit -m "Fixed version info in tag $VERSION" configure.ac
 	./autogen.sh; rm -rf aclocal.m4 autom4te.cache $( find -name .svn )
 	rm -f release.sh # don't include this script in the source tar
 
