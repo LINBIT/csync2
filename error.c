6ec8cef32b36f0890330b1f1490254b62c8781dd
Clifford Wolf <clifford@linbit.com>
2005-06-27 14:11:09 +0000

    Added blank after <PID> in output file

--- error.c
+++ error.c
@@ -29,7 +29,7 @@ void csync_fatal(const char *fmt, ...)
 	va_list ap;
 
 	if ( csync_server_child_pid )
-		fprintf(csync_debug_out, "<%d>", csync_server_child_pid);
+		fprintf(csync_debug_out, "<%d> ", csync_server_child_pid);
 
 	va_start(ap, fmt);
 	vfprintf(csync_debug_out, fmt, ap);
@@ -46,7 +46,7 @@ void csync_debug(int lv, const char *fmt, ...)
 	if ( csync_debug_level < lv ) return;
 
 	if ( csync_server_child_pid )
-		fprintf(csync_debug_out, "<%d>", csync_server_child_pid);
+		fprintf(csync_debug_out, "<%d> ", csync_server_child_pid);
 
 	va_start(ap, fmt);
 	vfprintf(csync_debug_out, fmt, ap);
