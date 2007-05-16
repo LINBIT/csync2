7d1779da0988903f5ed27716963e6a35ef8f8e5e
Clifford Wolf <clifford@linbit.com>
2007-05-16 17:42:38 +0000

    Do not use get_current_dir_name() on *BSD systems

--- getrealfn.c
+++ getrealfn.c
@@ -27,7 +27,7 @@
 
 static char *my_get_current_dir_name()
 {
-#ifdef __CYGWIN__
+#if defined __CYGWIN__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __NetBSD__
 	char *r = malloc(1024);
 	if (!getcwd(r, 1024))
 		strcpy(r, "/__PATH_TO_LONG__");
