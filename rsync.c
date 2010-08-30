894379e76e11081a1cf5f90c3df752d6587af547
Johannes Thoma <johannes.thoma@linbit.com>
2010-08-30 18:11:04 +0200

    Tempfiles are now created where the data files are located.
    By doing so, we save to copy the whole file after the patch operation,
    it has been replaced by a simple rename system call.

--- rsync.c
+++ rsync.c
@@ -30,10 +30,157 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/* for MAXPATHLEN */
+#include <sys/param.h>
+
 
 #ifdef __CYGWIN__
 #include <w32api/windows.h>
 #endif
+
+
+/* This has been taken from rsync:lib/compat.c */
+
+/**
+ * Like strncpy but does not 0 fill the buffer and always null
+ * terminates.
+ *
+ * @param bufsize is the size of the destination buffer.
+ *
+ * @return index of the terminating byte.
+ **/
+static size_t strlcpy(char *d, const char *s, size_t bufsize)
+{
+        size_t len = strlen(s);
+        size_t ret = len;
+        if (bufsize > 0) {
+                if (len >= bufsize)
+                        len = bufsize-1;
+                memcpy(d, s, len);
+                d[len] = 0;
+        }
+        return ret;
+}
+
+
+/* This has been taken from rsync sources: receiver.c */
+
+#define TMPNAME_SUFFIX ".XXXXXX"
+#define TMPNAME_SUFFIX_LEN ((int)sizeof TMPNAME_SUFFIX - 1)
+#define MAX_UNIQUE_NUMBER 999999
+#define MAX_UNIQUE_LOOP 100
+
+/* get_tmpname() - create a tmp filename for a given filename
+ *
+ * If a tmpdir is defined, use that as the directory to put it in.  Otherwise,
+ * the tmp filename is in the same directory as the given name.  Note that
+ * there may be no directory at all in the given name!
+ *
+ * The tmp filename is basically the given filename with a dot prepended, and
+ * .XXXXXX appended (for mkstemp() to put its unique gunk in).  We take care
+ * to not exceed either the MAXPATHLEN or NAME_MAX, especially the last, as
+ * the basename basically becomes 8 characters longer.  In such a case, the
+ * original name is shortened sufficiently to make it all fit.
+ *
+ * If the make_unique arg is True, the XXXXXX string is replaced with a unique
+ * string that doesn't exist at the time of the check.  This is intended to be
+ * used for creating hard links, symlinks, devices, and special files, since
+ * normal files should be handled by mkstemp() for safety.
+ *
+ * Of course, the only reason the file is based on the original name is to
+ * make it easier to figure out what purpose a temp file is serving when a
+ * transfer is in progress. */
+
+static int get_tmpname(char *fnametmp, const char *fname)
+{
+	int maxname, added, length = 0;
+	const char *f;
+	char *suf;
+
+	static unsigned counter_limit;
+	unsigned counter;
+
+	if ((f = strrchr(fname, '/')) != NULL) {
+		++f;
+		length = f - fname;
+		/* copy up to and including the slash */
+		strlcpy(fnametmp, fname, length + 1);
+	} else
+		f = fname;
+	fnametmp[length++] = '.';
+
+	/* The maxname value is bufsize, and includes space for the '\0'.
+	 * NAME_MAX needs an extra -1 for the name's leading dot. */
+	maxname = MIN(MAXPATHLEN - length - TMPNAME_SUFFIX_LEN,
+		      NAME_MAX - 1 - TMPNAME_SUFFIX_LEN);
+
+	if (maxname < 1) {
+		csync_debug(1, "temporary filename too long: %s\n", fname);
+		fnametmp[0] = '\0';
+		return 0;
+	}
+
+	added = strlcpy(fnametmp + length, f, maxname);
+	if (added >= maxname)
+		added = maxname - 1;
+	suf = fnametmp + length + added;
+
+	if (!counter_limit) {
+		counter_limit = (unsigned)getpid() + MAX_UNIQUE_LOOP;
+		if (counter_limit > MAX_UNIQUE_NUMBER || counter_limit < MAX_UNIQUE_LOOP)
+			counter_limit = MAX_UNIQUE_LOOP;
+
+		counter = counter_limit - MAX_UNIQUE_LOOP;
+
+		/* This doesn't have to be very good because we don't need
+		 * to worry about someone trying to guess the values:  all
+		 * a conflict will do is cause a device, special file, hard
+		 * link, or symlink to fail to be created.  Also: avoid
+		 * using mktemp() due to gcc's annoying warning. */
+		while (1) {
+			snprintf(suf, TMPNAME_SUFFIX_LEN+1, ".%d", counter);
+			if (access(fnametmp, 0) < 0)
+				break;
+			if (++counter >= counter_limit)
+				return 0;
+		}
+	} else
+		memcpy(suf, TMPNAME_SUFFIX, TMPNAME_SUFFIX_LEN+1);
+
+	return 1;
+}
+
+
+/* Returns open file handle for a temp file that resides in the
+   same directory as file fname. The file must be removed after
+   usage.
+*/
+
+static FILE *open_temp_file(char *fnametmp, const char *fname)
+{
+	FILE *f;
+	int fd;
+
+	if (get_tmpname(fnametmp, fname) == 0) {
+		csync_debug(1, "ERROR: Couldn't find tempname for file %s\n", fname);
+		return NULL;
+	}
+
+	f = NULL;
+	fd = open(fnametmp, O_CREAT | O_EXCL | O_RDWR, S_IWUSR | S_IRUSR);
+	if (fd >= 0) {
+		f = fdopen(fd, "wb+");
+			/* not unlinking since rename wouldn't work then */
+	}
+	if (fd < 0 || !f) {
+		csync_debug(1, "ERROR: Could not open result from tempnam(%s)!\n", fnametmp);
+		return NULL;
+	}
+
+	return f;
+}
+
+
 
 #ifdef _SVID_SOURCE
 static FILE *paranoid_tmpfile()
@@ -55,6 +202,7 @@ static FILE *paranoid_tmpfile()
 	if (fd < 0 || !f)
 		csync_fatal("ERROR: Could not open result from tempnam(%s)!\n", name);
 
+	csync_debug(3, "Tempfilename is %s\n", name);
 	free(name);
 	return f;
 }
@@ -150,18 +298,23 @@ int csync_rs_check(const char *filename, int isreg)
 	rs_stats_t stats;
 	rs_result result;
 	long size;
+	char tmpfname[MAXPATHLEN];
 
 	csync_debug(3, "Csync2 / Librsync: csync_rs_check('%s', %d [%s])\n",
 		filename, isreg, isreg ? "regular file" : "non-regular file");
 
 	csync_debug(3, "Opening basis_file and sig_file..\n");
 
-	sig_file = paranoid_tmpfile();
+	sig_file = open_temp_file(tmpfname, prefixsubst(filename));
 	if ( !sig_file ) goto io_error;
+	if (unlink(tmpfname) < 0) goto io_error;
 
 	basis_file = fopen(prefixsubst(filename), "rb");
-	if ( !basis_file ) basis_file = paranoid_tmpfile();
-	if ( !basis_file ) goto io_error;
+	if ( !basis_file ) {  /* ?? why a tmp file? */
+		basis_file = open_temp_file(tmpfname, prefixsubst(filename));
+		if ( !basis_file ) goto io_error;
+		if (unlink(tmpfname) < 0) goto io_error;
+	}
 
 	if ( isreg ) {
 		csync_debug(3, "Running rs_sig_file() from librsync....\n");
@@ -235,14 +388,19 @@ error:;
 
 void csync_rs_sig(const char *filename)
 {
-	FILE *basis_file, *sig_file;
+	FILE *basis_file = 0, *sig_file = 0;
 	rs_stats_t stats;
 	rs_result result;
+	char tmpfname[MAXPATHLEN];
 
 	csync_debug(3, "Csync2 / Librsync: csync_rs_sig('%s')\n", filename);
 
 	csync_debug(3, "Opening basis_file and sig_file..\n");
-	sig_file = paranoid_tmpfile();
+
+	sig_file = open_temp_file(tmpfname, prefixsubst(filename));
+	if ( !sig_file ) goto io_error;
+	if (unlink(tmpfname) < 0) goto io_error;
+
 	basis_file = fopen(prefixsubst(filename), "rb");
 	if ( !basis_file ) basis_file = fopen("/dev/null", "rb");
 
@@ -258,19 +416,34 @@ void csync_rs_sig(const char *filename)
 	csync_debug(3, "Signature has been created successfully.\n");
 	fclose(basis_file);
 	fclose(sig_file);
+
+	return;
+
+io_error:
+	csync_debug(0, "I/O Error '%s' in rsync-sig: %s\n",
+			strerror(errno), prefixsubst(filename));
+
+	if (basis_file) fclose(basis_file);
+	if (sig_file) fclose(sig_file);
 }
+
+
 
 int csync_rs_delta(const char *filename)
 {
-	FILE *sig_file, *new_file, *delta_file;
+	FILE *sig_file = 0, *new_file = 0, *delta_file = 0;
 	rs_result result;
 	rs_signature_t *sumset;
 	rs_stats_t stats;
+	char tmpfname[MAXPATHLEN];
 
 	csync_debug(3, "Csync2 / Librsync: csync_rs_delta('%s')\n", filename);
 
 	csync_debug(3, "Receiving sig_file from peer..\n");
-	sig_file = paranoid_tmpfile();
+	sig_file = open_temp_file(tmpfname, prefixsubst(filename));
+	if ( !sig_file ) goto io_error;
+	if (unlink(tmpfname) < 0) goto io_error;
+
 	if ( csync_recv_file(sig_file) ) {
 		fclose(sig_file);
 		return -1;
@@ -291,7 +464,10 @@ int csync_rs_delta(const char *filename)
 		errno = backup_errno;
 		return -1;
 	}
-	delta_file = paranoid_tmpfile();
+
+	delta_file = open_temp_file(tmpfname, prefixsubst(filename));
+	if ( !delta_file ) goto io_error;
+	if (unlink(tmpfname) < 0) goto io_error;
 
 	csync_debug(3, "Running rs_build_hash_table() from librsync..\n");
 	result = rs_build_hash_table(sumset);
@@ -312,6 +488,16 @@ int csync_rs_delta(const char *filename)
 	fclose(new_file);
 
 	return 0;
+
+io_error:
+	csync_debug(0, "I/O Error '%s' in rsync-delta: %s\n",
+			strerror(errno), prefixsubst(filename));
+
+	if (new_file) fclose(new_file);
+	if (delta_file) fclose(delta_file);
+	if (sig_file) fclose(sig_file);
+
+	return -1;
 }
 
 int csync_rs_patch(const char *filename)
@@ -323,21 +509,26 @@ int csync_rs_patch(const char *filename)
 	char buffer[512];
 	char *errstr = "?";
 	int rc;
+	char tmpfname[MAXPATHLEN], newfname[MAXPATHLEN];
 
 	csync_debug(3, "Csync2 / Librsync: csync_rs_patch('%s')\n", filename);
 
 	csync_debug(3, "Receiving delta_file from peer..\n");
-	delta_file = paranoid_tmpfile();
+	delta_file = open_temp_file(tmpfname, prefixsubst(filename));
 	if ( !delta_file ) { errstr="creating delta temp file"; goto io_error; }
+	if (unlink(tmpfname) < 0) { errstr="removing delta temp file"; goto io_error; }
 	if ( csync_recv_file(delta_file) ) goto error;
 
 	csync_debug(3, "Opening to be patched file on local host..\n");
 	basis_file = fopen(prefixsubst(filename), "rb");
-	if ( !basis_file ) basis_file = paranoid_tmpfile();
-	if ( !basis_file ) { errstr="opening data file for reading"; goto io_error; }
+	if ( !basis_file ) {
+		basis_file = open_temp_file(tmpfname, prefixsubst(filename));
+		if ( !basis_file ) { errstr="opening data file for reading"; goto io_error; }
+		if (unlink(tmpfname) < 0) { errstr="removing data temp file"; goto io_error; }
+	}
 
 	csync_debug(3, "Opening temp file for new data on local host..\n");
-	new_file = paranoid_tmpfile();
+	new_file = open_temp_file(newfname, prefixsubst(filename));
 	if ( !new_file ) { errstr="creating new data temp file"; goto io_error; }
 
 	csync_debug(3, "Running rs_patch_file() from librsync..\n");
@@ -347,12 +538,12 @@ int csync_rs_patch(const char *filename)
 		goto error;
 	}
 
-	csync_debug(3, "Copying new data to local file..\n");
+	csync_debug(3, "Renaming tmp file to data file..\n");
 	fclose(basis_file);
-	rewind(new_file);
-	unlink(prefixsubst(filename));
 
 #ifdef __CYGWIN__
+
+/* TODO: needed? */
 	// This creates the file using the native windows API, bypassing
 	// the cygwin wrappers and so making sure that we do not mess up the
 	// permissions..
@@ -381,14 +572,9 @@ int csync_rs_patch(const char *filename)
 	}
 #endif
 
-	basis_file = fopen(prefixsubst(filename), "wb");
-	if ( !basis_file ) { errstr="opening data file for writing"; goto io_error; }
-
-	while ( (rc = fread(buffer, 1, 512, new_file)) > 0 )
-		fwrite(buffer, rc, 1, basis_file);
+	if (rename(newfname, prefixsubst(filename)) < 0) { errstr="renaming tmp file to to be patched file"; goto io_error; }
 
 	csync_debug(3, "File has been patched successfully.\n");
-	fclose(basis_file);
 	fclose(delta_file);
 	fclose(new_file);
 
