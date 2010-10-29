8054dc410d2179bf10ba2a813b2dc7ec07301ad0
Johannes Thoma <johannes.thoma@linbit.com>
2010-10-29 14:22:19 +0200

    Added dl.h which I forgot in the last commits.

---
+++ dl.h
@@ -0,0 +1,12 @@
+#ifndef DL_H
+#define DL_H
+
+#include <dlfcn.h>
+
+#define LOOKUP_SYMBOL(dl_handle, sym) \
+	f.sym ## _fn = dlsym(dl_handle, #sym); \
+	if ((f.sym ## _fn) == NULL) { \
+		csync_fatal ("Could not lookup %s in libpq.so: %s\n", #sym, dlerror()); \
+	}
+
+#endif
