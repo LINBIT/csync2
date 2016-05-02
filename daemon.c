175a01c570b0ca61738e70d872aacf267de3b92c
Lars Ellenberg <lars.ellenberg@linbit.com>
2016-05-02 17:07:18 +0200

    fix log message when generating backup files, demote log level
    log level of an "expected result" message should not be warning.
    
    Also use consistent octal format for unix permissions,
    and add a missing newline.

--- daemon.c
+++ daemon.c
@@ -264,13 +264,13 @@ int csync_setBackupFileStatus(char *filename, int backupDirLength)
 	struct stat buf;
 	int rc = stat((filename + backupDirLength), &buf);
 	if (rc == 0) {
-		csync_debug(0, "Stating original file %s rc: %d mode: %o", (filename + backupDirLength), rc, buf.st_mode);
+		csync_debug(1, "Stating original file %s rc: %d mode: %o \n", (filename + backupDirLength), rc, buf.st_mode);
 
 		rc = chown(filename, buf.st_uid, buf.st_gid);
-		csync_debug(0, "Changing owner of %s to user %d and group %d, rc= %d \n", filename, buf.st_uid, buf.st_gid, rc);
+		csync_debug(rc == 0, "Changing owner of %s to user %d and group %d, rc= %d \n", filename, buf.st_uid, buf.st_gid, rc);
 
 		rc = chmod(filename, buf.st_mode);
-		csync_debug(0, "Changing mode of %s to mode %d, rc= %d \n", filename, buf.st_mode, rc);
+		csync_debug(rc == 0, "Changing mode of %s to mode %o, rc= %d \n", filename, buf.st_mode, rc);
 
 	} else {
 		csync_debug(0, "Error getting mode and owner ship from %s \n", (filename + backupDirLength));
